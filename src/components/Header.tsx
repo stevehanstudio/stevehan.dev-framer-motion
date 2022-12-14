14329013998a858f362e73713c8c3a042ced33b9
Steve Han <15229911+stevehanstudio@users.noreply.github.com>
2022-12-14 15:49:12 -0800

    Change header from position sticky to position fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,13 +9,11 @@ const Header = () => {
 
 	// console.log('In Header isMobile', isMobile)
 	return (
-		<header className='sticky top-0 left-0 z-50 flex flex-col w-full md:px-4 md:pt-4 md:mx-auto bg-sweetAiran dark:bg-laborWorker md:place-content-center md:fade-bottom'>
+		<header className='fixed top-0 left-0 z-50 flex flex-col w-full md:px-4 md:pt-4 md:mx-auto bg-sweetAiran dark:bg-laborWorker md:place-content-center md:fade-bottom'>
 			{/* <header className='absolute top-0 left-0 z-10 flex flex-col w-full p-4 mx-auto bg-gray-700 dark:bg-gray-700 place-content-center'> */}
 			{ isMobile
-				?	<MobileMenu
-				/>
-				: <Menu
-				/>
+				?	<MobileMenu />
+				: <Menu	/>
 			}
 		</header>
 	)
